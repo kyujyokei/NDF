6cad2ca859e7dee8ba8e1a67da1e7362709a8f7b
kyujyokei <kyujyokei@gmail.com>
2019-01-17 17:31:26 -0800

    Implemented transfer learning

--- local/main.js
+++ local/main.js
@@ -3,6 +3,10 @@ let mobileNet;
 let video;
 let label='model loading...';
 //let results;
+let classifier;
+let button1;
+let button2;
+let button3;
 
 
 //setup function
@@ -16,13 +20,48 @@ function setup()
     video.hide();
 
     //initialize the mobilenet object with a callback
-    mobileNet= ml5.imageClassifier('MobileNet',video,ModelLoaded);
+    // mobileNet = ml5.imageClassifier('MobileNet',video,modelLoaded);
+    mobileNet = ml5.featureExtractor('MobileNet', modelReady);
+
+    classifier = mobileNet.classification(video, videoReady);
+
+    button1 = createButton("Stapler");
+    button1.mousePressed( function () {
+        classifier.addImage("Stapler")
+    });
+
+    button2 = createButton("Eraser");
+    button2.mousePressed( function () {
+        classifier.addImage("Eraser")
+    });
+
+    button3 = createButton("Train");
+    button3.mousePressed( function () {
+        classifier.train(whileTraining);
+    });
+
 
 }
 
+function whileTraining (loss) {
+    if (loss == null) {
+        console.log('Training is complete');
+        classifier.classify(result);
+    }
+    else {
+        console.log(loss);
+    }
+}
+
+function modelReady()
+{
+    console.log('Model is ready');
+    //predicting the image
+    // mobileNet.predict(result)
+}
 
 //callback function for when the model is ready for prediction
-function ModelLoaded()
+function modelLoaded()
 {
     console.log('Model is ready');
     //predicting the image
@@ -40,12 +79,18 @@ function result(err,res)
     else{
        
         //get the label from the json result
-        label = res[0].className;
+        label = res;
        
     //predicting the image again
-         mobileNet.predict(result)
+        //  mobileNet.predict(result)
+        classifier.classify(result);
     }
 }
+
+function videoReady () {
+
+}
+
 function draw()
 {
     //Displaying the webcam stream
